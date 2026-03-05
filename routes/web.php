0ab7be04e0a81cbfecc7dfecf7ea5f8365e6bf1e
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-03-06 01:20:26 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/projects', \App\Livewire\Website\Projects\Projects::class)->name('p
 Route::get('/projects/{slug}', \App\Livewire\Website\Projects\ProjectDetails::class)->name('projects.details');
 Route::get('/offers', \App\Livewire\Website\Offers\Offers::class)->name('offers');
 Route::get('/blogs',\App\Livewire\Website\Blogs\Blogs::class)->name('blogs');
-Route::get('/contact-us',\App\Livewire\Website\contact\ContactUs::class)->name('contact');
+Route::get('/contact-us',\App\Livewire\Website\Contact\ContactUs::class)->name('contact');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
