587198409a6674a0d44b9497502027d19a50e9f3
wholeomarfaruk <wholeomarfaruk@gmail.com>
2026-03-17 06:57:22 +0600

    update

--- resources/views/livewire/admin/dashboard/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard/dashboard.blade.php
@@ -47,13 +47,15 @@
                                        </svg><span class="text-xs text-green-600 font-medium">+12.5%</span>
                                     </div> --}}
                                </div>
-                               <div class=" p-3 rounded-full bg-blue-500"><svg xmlns="http://www.w3.org/2000/svg"
-                                       width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                       stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                       stroke-linejoin="round" class="lucide lucide-dollar-sign h-6 w-6 text-white">
-                                       <line x1="12" x2="12" y1="2" y2="22"></line>
-                                       <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                                   </svg></div>
+                               <div class=" p-3 rounded-full bg-blue-500">
+                                
+
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"  class="lucide lucide-dollar-sign h-6 w-6 text-white">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
+                                </div>
                            </div>
                        </div>
                    </div>
@@ -74,13 +76,14 @@
                                        </svg><span class="text-xs text-green-600 font-medium">+3.2%</span>
                                     </div> --}}
                                </div>
-                               <div class="p-3 rounded-full bg-green-500"><svg xmlns="http://www.w3.org/2000/svg"
-                                       width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                       stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                       stroke-linejoin="round" class="lucide lucide-dollar-sign h-6 w-6 text-white">
-                                       <line x1="12" x2="12" y1="2" y2="22"></line>
-                                       <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                                   </svg></div>
+                               <div class="p-3 rounded-full bg-green-500">
+                                
+                                
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="lucide lucide-dollar-sign h-6 w-6 text-white">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+</svg>
+
+                                </div>
                            </div>
                        </div>
                    </div>
@@ -101,13 +104,13 @@
                                        </svg><span class="text-xs text-green-600 font-medium">+5.8%</span>
                                     </div> --}}
                                </div>
-                               <div class="p-3 rounded-full bg-purple-500"><svg xmlns="http://www.w3.org/2000/svg"
-                                       width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                       stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                       stroke-linejoin="round" class="lucide lucide-dollar-sign h-6 w-6 text-white">
-                                       <line x1="12" x2="12" y1="2" y2="22"></line>
-                                       <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                                   </svg></div>
+                               <div class="p-3 rounded-full bg-purple-500">
+                           
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="lucide lucide-dollar-sign h-6 w-6 text-white">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+</svg>
+
+                                </div>
                            </div>
                        </div>
                    </div>
@@ -129,13 +132,11 @@
                                     </div> --}}
                                </div>
                                <div class="p-3 rounded-full bg-orange-500">
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                       viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                       stroke-linecap="round" stroke-linejoin="round"
-                                       class="lucide lucide-dollar-sign h-6 w-6 text-white">
-                                       <line x1="12" x2="12" y1="2" y2="22"></line>
-                                       <path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
-                                   </svg>
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="lucide lucide-dollar-sign h-6 w-6 text-white">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+</svg>
+
+
                                </div>
                            </div>
                        </div>
