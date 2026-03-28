6b824eb09d5da784e54e13e8d9da437c20391a60
unknown <wholeomarfaruk@gmail.com|git config --global user.email>
2026-03-28 14:52:48 +0600

    add meta title, icon

--- resources/views/layouts/admin/admin.blade.php
+++ resources/views/layouts/admin/admin.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Admin Panel</title>
+    <link rel="shortcut icon" href="{{asset('assets/logo/sud-logo.png')}}" type="image/x-icon">
     @vite(['resources/sass/admin.scss', 'resources/css/admin.css', 'resources/js/admin.js'])
     @livewireStyles
     @stack('styles')
